7e4f09b3fe2370a263b91e6c1ec8594aae7b2316
Jason Leong <70220705+JayzleeProgrammer@users.noreply.github.com>
2024-02-17 23:31:08 -0600

    Added Some More Styling!

--- CollegeApp/screens/Home.tsx
+++ CollegeApp/screens/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
@@ -28,11 +28,31 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Home Screen!</Text>
-      <Button title="Logout" onPress={handleLogout} />
-      <Button title="Choice1Test" onPress={handleChoice1TestChange} />
-      <Button title="Choice2Test" onPress={handleChoice2TestChange} />
-      <Button title="Choice3Test" onPress={handleChoice3TestChange} />
+      
+        <Text style={styles.topCorner}>SWE Capstone Team NULL</Text>
+      
+      <View style={styles.logout}>
+        <Button title="Logout" onPress={handleLogout} />
+      </View>
+      
+      <Text style={styles.title}>Welcome to My College Experience!</Text>
+
+      <Image
+        source={{uri: 'https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
+        style={{width: 400, height: 400}}></Image>
+
+      <Text style={styles.title}>Choose From the Following:</Text>
+
+      <View style = {styles.button}>
+        <Button title="Choice1Test" onPress={handleChoice1TestChange} />
+      </View>
+      <View style = {styles.button}>
+        <Button title="Choice2Test" onPress={handleChoice2TestChange} />
+      </View>
+      <View style = {styles.button}>
+        <Button title="Choice3Test" onPress={handleChoice3TestChange} />
+      </View>
+      
     </View>
     
   );
@@ -43,12 +63,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 20,
+   
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
+    top: 20,
+    fontWeight: 'bold',
   },
-});
+
+  button: {
+    fontSize: 24,
+    marginBottom: 20,
+    padding: 0,
+  },
+  logout: {
+    position: 'absolute',
+    top:5,
+    right:10,
+    height: 40,
+    
+  },
+  topCorner: {
+    position: 'absolute',
+    top:5,
+    left:10,
+    fontWeight: 'bold',
+  },
+  });
 
 export default Home;
