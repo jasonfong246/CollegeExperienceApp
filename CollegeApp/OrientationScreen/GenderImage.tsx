0572ea476b186c8d602b39bedfce15a1aa384366
Jason Leong <70220705+JayzleeProgrammer@users.noreply.github.com>
2024-04-10 00:46:36 -0500

    Changed Positioning

--- CollegeApp/OrientationScreen/GenderImage.tsx
+++ CollegeApp/OrientationScreen/GenderImage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 const GenderImage = ({ gender }) => {
   // Define the image sources based on gender
-  const PlayerMale1=require("./malePlayer.png");
-    const PlayerFemale1=require("./PlayerFemale1.png");
-    const PlayerMale2=require("./PlayerMale.png");
-    const PlayerFemale2=require("./PlayerFemale2.png");
+  const PlayerMale1 = require("./malePlayer.png");
+  const PlayerFemale1 = require("./PlayerFemale1.png");
+  const PlayerMale2 = require("./PlayerMale.png");
+  const PlayerFemale2 = require("./PlayerFemale2.png");
 
   // Choose the image source based on the gender value
   let imageSource;
@@ -14,25 +14,37 @@ const GenderImage = ({ gender }) => {
   switch (gender) {
     case 'male':
       imageSource = PlayerMale1;
-      imageSource2=PlayerMale2;
+      imageSource2 = PlayerMale2;
       break;
     case 'female':
       imageSource = PlayerFemale1;
-      imageSource2=PlayerFemale2;
+      imageSource2 = PlayerFemale2;
       break;
     default:
       imageSource = PlayerFemale2;
-      imageSource2=PlayerMale2;
+      imageSource2 = PlayerMale2;
       break;
   }
 
   return (
-    <View>
-      {/* Display the image */}
-      <Image source={imageSource} style={{ flex: 1,resizeMode: 'contain', }} />
-      <Image source={imageSource2} style={{ flex: 1,resizeMode: 'contain', }} />
+    <View style={styles.container}>
+      {/* Display the images */}
+      <Image source={imageSource} style={styles.image} />
+      <Image source={imageSource2} style={styles.image} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 200, // Adjust the width as needed
+    height: 200, // Adjust the height as needed
+    resizeMode: 'contain',
+    marginBottom: 10, // Adjust the margin between images as needed
+  },
+});
 
 export default GenderImage;
