5e1c8b5aeb397f170ab8ee5f610717da5c4f17ba
Jason Leong <70220705+JayzleeProgrammer@users.noreply.github.com>
2024-04-10 00:21:48 -0500

    If someone could help with the weird styling and add the image to the Database, that'd help!

--- CollegeApp/OrientationScreen/GenderImage.tsx
+++ CollegeApp/OrientationScreen/GenderImage.tsx
@@ -5,29 +5,32 @@ const GenderImage = ({ gender }) => {
   // Define the image sources based on gender
   const PlayerMale1=require("./malePlayer.png");
     const PlayerFemale1=require("./PlayerFemale1.png");
-    const PlayerMale2=require("./MalePlayer2.png");
+    const PlayerMale2=require("./PlayerMale.png");
     const PlayerFemale2=require("./PlayerFemale2.png");
 
   // Choose the image source based on the gender value
   let imageSource;
+  let imageSource2;
   switch (gender) {
     case 'male':
       imageSource = PlayerMale1;
+      imageSource2=PlayerMale2;
       break;
     case 'female':
       imageSource = PlayerFemale1;
+      imageSource2=PlayerFemale2;
       break;
     default:
       imageSource = PlayerFemale2;
+      imageSource2=PlayerMale2;
       break;
   }
 
   return (
     <View>
       {/* Display the image */}
-      <Image source={imageSource} style={{ flex: 1,
-            
-            resizeMode: 'contain', }} />
+      <Image source={imageSource} style={{ flex: 1,resizeMode: 'contain', }} />
+      <Image source={imageSource2} style={{ flex: 1,resizeMode: 'contain', }} />
     </View>
   );
 };
